ead3ee6fcac32eb220dc80db851e03a7739272fc
April Shen <april.tuesday@gmail.com>
2020-12-03 10:17:23 +0000

    part 2 of day 1

--- day-01.sh
+++ day-01.sh
@@ -1,13 +1,36 @@
-#!/bin/bash
-diffs=()
+#!/usr/local/bin/bash
+# ^ for associative arrays, need bash4+
+diffs1=()
+declare -A diffs2
 while read -r line
 do
+	# part 1
   val=$((line+0))
-  key=$((2020-val))
-  if [[ " ${diffs[@]} " =~ " ${val} " ]]
+  diff=$((2020-val))
+  if [[ "${diffs1[@]}" =~ "${val}" ]]
   then
-  	echo $((key*val))
-  	break
+  	echo "Part 1: $((diff*val))"
+  	# break
   fi
-  diffs+=($key)
+  diffs1+=($diff)
+
+  # part 2
+  for k in "${!diffs2[@]}"
+  do
+  	# 1. if val is in the list, return k*val*(2020-k-val)
+  	# 2. otherwise append 2020-k-val to the list
+  	IFS=',' read -r -a list <<< "${diffs2[$k]}"
+  	newval=$((2020-k-val))
+  	if [[ "${list[@]}" =~ "${val}" ]]
+  	then
+  		echo "Part 2: $((k*val*newval))"
+  		# break
+  	else
+  		if [[ $newval -gt 0 ]]
+  		then
+	  		diffs2[$k]+=",$newval"
+	  	fi
+	  fi
+  done
+  diffs2[$val]=""
 done
