915dc173a14acc0e5929d7d5252277a9b9686869
April Shen <april.tuesday@gmail.com>
2020-12-07 20:41:56 +0000

    day 7 part 2

--- day-07.sh
+++ day-07.sh
@@ -14,6 +14,11 @@ global_rematch() {
 #   node -> comma-separated string of children
 #   e.g. 'shiny gold' -> 'bright white,muted yellow'
 declare -A tree
+
+# Part 2
+# b1,b2 -> number of b1s contained in b2
+declare -A nums
+
 node_regex="(.*) bags contain"
 parent_regex="[0-9]+ ([^0-9]*) bag"
 while read -r line
@@ -30,11 +35,17 @@ do
 			else
 				tree[$parent]+=",$child"
 			fi
+			# get the number of parents contained in child
+			num_regex="([0-9]+) $parent bag"
+			if [[ $line =~ $num_regex ]]
+			then
+				nums["$parent,$child"]="${BASH_REMATCH[1]}"
+			fi
 		done <<< $(global_rematch "$line" "$parent_regex")
 	fi
 done
 
-# Gets list of all nodes reachable from a given root
+# Gets list of all nodes in subtree for a given root
 visit_nodes () {
 	local current="$1"
 	local visited="$2"  # comma-sep string of visited nodes
@@ -59,8 +70,35 @@ visit_nodes () {
 	echo $visited
 }
 
-# Count nodes in subtree rooted at 'shiny gold' (excluding root)
-visited=$(visit_nodes "shiny gold" "")
+# Counts all the bags backwards from root
+# NOTE: this is not a good solution.
+backwards_count () {
+	local current="$1"
+	total=1  # include self
+	for key in "${!tree[@]}"
+	do
+		IFS=',' read -r -a children <<< "${tree[$key]}"
+		for c in "${children[@]}"
+		do
+			if [[ "$c" == "$current" ]]
+			then
+				x=${nums["$key,$current"]}
+				y=$(backwards_count "$key")
+				total=$((total+x*y))
+			fi
+		done
+	done
+	echo $total
+}
+
+root="shiny gold"
+
+# part 1
+visited=$(visit_nodes "$root" "")
 IFS=',' read -r -a nodes <<< "$visited"
-total=${#nodes[@]}  # includes root
-echo "Part 1: $((total-1))"
+total1=${#nodes[@]}  # includes root
+echo "Part 1: $((total1-1))"
+
+# part 2
+total2=$(backwards_count "$root")  # includes root
+echo "Part 2: $((total2-1))"
